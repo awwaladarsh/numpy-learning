9f8c3b36cc63ef84254b2ff1c1ad210fd7ae0536
awwaladarsh <awwaladarsh@yahoo.com>
2026-07-11 12:27:35 -0700

    Add example for numpy column_stack usage
    Demonstrate the use of np.column_stack to combine two matrices.

--- column_stack.py
+++ column_stack.py
@@ -12,3 +12,18 @@ print(result)
 # [[1 4]
 #  [2 5]
 #  [3 6]]
+
+
+import numpy as np
+
+matrix_a = np.array([[1, 2], 
+                     [3, 4]])
+matrix_b = np.array([[5], 
+                     [6]])
+
+result = np.column_stack((matrix_a, matrix_b))
+
+print(result)
+# Output:
+# [[1 2 5]
+#  [3 4 6]]
